44f277bcfa1c5c13c68ceb1c6c58fc4a4f7d1d36
ismet <iozozturk@gmail.com>
2015-12-18 17:23:03 +0200

    Bodyparser accepts forms in order to eliminate boilerplate at form bindings

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -8,10 +8,11 @@ import com.mongodb.MongoWriteException
 import forms.AuthForms
 import models.User
 import play.api.Play.current
+import play.api.data.Form
 import play.api.i18n.Messages.Implicits._
 import play.api.i18n.MessagesApi
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.mvc.{Action, BodyParsers, Controller}
+import play.api.mvc.{Action, BodyParser, Controller}
 import services.UserService
 
 import scala.concurrent.Future
@@ -22,32 +23,31 @@ import scala.concurrent.Future
 class AuthController @Inject()(userService: UserService,
                                val messagesApi: MessagesApi) extends Controller {
 
-  def signup = Action.async(BodyParsers.parse.json) { implicit request =>
-    AuthForms.SignupForm.bindFromRequest().fold(
-      errorForm => {
-        Future {
-          BadRequest(errorForm.errorsAsJson)
-        }
-      },
-      signupForm => {
-        val user = User(UUID.randomUUID(),
-          signupForm.name,
-          signupForm.email,
-          null,
-          signupForm.username,
-          signupForm.password.bcrypt,
-          System.currentTimeMillis())
+  def signup = Action.async(BodyParser { implicit request => parse.form(AuthForms.SignupForm, onErrors = { errorForm: Form[AuthForms.SignupData] => BadRequest(errorForm.errorsAsJson) })(request) }) { implicit request =>
 
-        userService.save(user).toFuture().map((_) => {
-          Ok
-        }).recoverWith {
-          case e: MongoWriteException => Future {
-            Forbidden
-          }
-        }
+    val signupData = request.body
 
+    val user = User(UUID.randomUUID(),
+      signupData.name,
+      signupData.email,
+      null,
+      signupData.username,
+      signupData.password.bcrypt,
+      System.currentTimeMillis())
+
+    userService.save(user).toFuture().map((_) => {
+      Ok
+    }).recoverWith {
+      case e: MongoWriteException => Future {
+        Forbidden
       }
-    )
+    }
+
   }
+
+  def login = TODO
+
+  def logout = TODO
+
 
 }
