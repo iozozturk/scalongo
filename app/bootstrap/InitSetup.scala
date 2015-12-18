2ac2a49744035e58b60ebacbf2cd6377c25aa96a
ismet <iozozturk@gmail.com>
2015-12-18 13:45:46 +0200

    email unique index added on User collection

--- app/bootstrap/InitSetup.scala
+++ app/bootstrap/InitSetup.scala
@@ -12,13 +12,21 @@ import services.Mongo
   */
 class InitSetup @Inject()(mongo: Mongo) {
 
+  def preMessages(): Unit = {
+    Logger.info("Bootstrapping application..")
+  }
+
   def defineIndexes(): Unit = {
     val options: IndexOptions = new IndexOptions().unique(true)
     mongo.db.getCollection("user").createIndex(ascending("username"), options).subscribe((s: String) => {
-      Logger.info("User index created")
+      Logger.info("username index created on User collection")
+    })
+    mongo.db.getCollection("user").createIndex(ascending("email"), options).subscribe((s: String) => {
+      Logger.info("email index created on User collection")
     })
   }
 
+  preMessages()
   defineIndexes()
 
 }
