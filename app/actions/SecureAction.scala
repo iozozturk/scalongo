8e45dd543d0557525edb515f9edf843ab8ec3316
ismet <iozozturk@gmail.com>
2015-12-24 00:18:52 +0200

    Login fix for exceptions at getting cookie value (dirty fix, should improve)

--- app/actions/SecureAction.scala
+++ app/actions/SecureAction.scala
@@ -1,7 +1,8 @@
 package actions
 
-import com.google.inject.{Singleton, Inject}
+import com.google.inject.{Inject, Singleton}
 import models.User
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 import services.{SessionService, UserService}
 
@@ -10,7 +11,7 @@ import scala.concurrent.Future
 /**
   * Created by ismet on 21/12/15.
   */
-class UserRequest[A](val user: Future[User], request: Request[A]) extends WrappedRequest[A](request) {
+class UserRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request) {
   def sessionId = request.cookies.get("sessionId").get.value
 }
 
@@ -19,16 +20,13 @@ class SecureAction @Inject()(sessionService: SessionService,
                              userService: UserService) extends ActionBuilder[UserRequest] with ActionRefiner[Request, UserRequest] {
 
   override protected def refine[A](request: Request[A]): Future[Either[Result, UserRequest[A]]] = {
-
-    val result = request.cookies.get("sessionId")
-      .map { sessionId =>
-        sessionService.updateLastActivity(sessionId.value)
-        sessionService.findUserBySessionId(sessionId.value)
+    sessionService
+      .findUserBySessionId(request.cookies.get("sessionId").getOrElse[Cookie](Cookie("", "")).value)
+      .map(u =>
+        Right(new UserRequest[A](u, request))
+      )
+      .recover {
+        case _ => Left[Result, UserRequest[A]](Results.Forbidden)
       }
-      .map(user => new UserRequest[A](user, request))
-      .toRight(left = Results.Forbidden)
-
-    Future.successful(result)
   }
-
 }
