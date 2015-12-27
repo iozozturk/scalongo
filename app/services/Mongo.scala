c6462b97c84ff24c32b6aa5e338415d56e4883c3
ismet <iozozturk@gmail.com>
2015-12-27 14:48:27 +0200

    database name is registered at application.conf

--- app/services/Mongo.scala
+++ app/services/Mongo.scala
@@ -3,8 +3,8 @@ package services
 import com.google.inject.{Inject, Singleton}
 import org.mongodb.scala.{MongoClient, MongoDatabase}
 import play.Logger
+import play.api.Configuration
 import play.api.inject.ApplicationLifecycle
-
 
 import scala.concurrent.Future
 
@@ -12,11 +12,13 @@ import scala.concurrent.Future
   * Created by ismet on 07/12/15.
   */
 @Singleton
-class Mongo @Inject()(applicationLifecycle: ApplicationLifecycle) {
+class Mongo @Inject()(applicationLifecycle: ApplicationLifecycle, configuration: Configuration) {
 
   val client: MongoClient = MongoClient()
 
-  val db: MongoDatabase = client.getDatabase("scalongo")
+  private val dbName: String = configuration.getString("mongo.db.name").get
+
+  val db: MongoDatabase = client.getDatabase(dbName)
 
   applicationLifecycle.addStopHook(() => {
     Logger.warn("Closing Mongo connection")
