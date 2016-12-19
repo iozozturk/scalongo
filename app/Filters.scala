beef492385e87bbd4e7674b8e062252e6dbd9e28
ismet özöztürk <iozozturk@gmail.com>
2016-12-19 23:45:03 +0300

    Registering Exception filter

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,14 +1,15 @@
 import javax.inject.Inject
 
-import filters.LoggingFilter
+import filters.{ExceptionFilter, LoggingFilter}
 import play.api.http.HttpFilters
 
 /**
   * Created by ismet on 16/12/15.
   */
 class Filters @Inject()(
-                         log: LoggingFilter
+                         log: LoggingFilter,
+                         exceptionFilter: ExceptionFilter
                        ) extends HttpFilters {
 
-  val filters = Seq(log)
+  val filters = Seq(log, exceptionFilter)
 }
